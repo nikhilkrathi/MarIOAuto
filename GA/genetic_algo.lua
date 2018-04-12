a8659e9e3a09b8885038d49e81bccfffb57f1ce9
Rohith Srivathsav <rohithroxtar@gmail.com>
2018-04-12 22:32:19 +0530

    extract top x% from table in genetic_algo.lua

--- GA/genetic_algo.lua
+++ GA/genetic_algo.lua
@@ -4,6 +4,17 @@ require "candidate"
 require "other_utils"
 
 function ga_crossover(tbl, topperc)
+    --extract top x perc from table
+    local top = {};
+    local top_max_ind = math.floor(topperc*(#tbl));
+    local top_max_cont = #(tbl[1].inputs);
+    for i=1, top_max_ind do
+        top[i] = gen_candidate.new();
+        for j=1, top_max_cont do
+            top[i].inputs[j] = deepcopy(tbl[i].inputs[j]);
+            top[i].input_fit = tbl[i].input_fit;
+        end
+    end
 end
 
 
