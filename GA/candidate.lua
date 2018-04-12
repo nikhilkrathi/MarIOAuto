c6b2132ec286b23c4f177228ded8b7ca80692adc
nikhilkrathi <nikhilkrathi@gmail.com>
2018-04-13 02:39:00 +0530

    candidate.lua file created.

---
+++ GA/candidate.lua
@@ -0,0 +1,11 @@
+--Candidate File
+
+gen_candidate = {
+    time = 0,
+    fitness = 0, 
+    has_won = false,
+    been_modified = true,
+    win_time = 0
+}
+gen_candidate.__index = gen_candidate;
+
