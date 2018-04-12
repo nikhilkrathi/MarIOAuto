59087ddfac4d9beba9f7557d0c6b6861a72acb9d
nikhilkrathi <nikhilkrathi@gmail.com>
2018-04-13 03:35:17 +0530

    deepcopy() function added in utilities file

--- GA/other_utils.lua
+++ GA/other_utils.lua
@@ -31,3 +31,36 @@ function contains_winner(table)
     end
     return false;
 end
+
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
+function bti(bl)
+    return (bl and 1) or 0
+end
+
+function btis(bl)
+    return (bl and "1") or "0"
+end
+
+function ctrl_tbl_btis(tbl)
+    return (
+        btis( tbl.up    )..
+        btis( tbl.down  )..
+        btis( tbl.left  )..
+        btis( tbl.right )..
+        btis( tbl.A     )..
+        btis( tbl.B     ))
+end
